91af6ae201952c8405987f0203fb319467dde118
Hrududu8 <rkorde@gmail.com>
2015-11-14 15:38:47 -0500

    Initial Commit

---
+++ Mass Combat2/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Mass Combat2
+//
+//  Created by rukesh on 11/14/15.
+//  Copyright © 2015 Rukesh. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
